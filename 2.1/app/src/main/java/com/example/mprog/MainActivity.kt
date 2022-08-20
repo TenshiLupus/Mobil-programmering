9b65839e10cbafd3834e2599afec0707a42d76ce
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-08-20 21:38:45 +0200

    code refactor

--- 2.1/app/src/main/java/com/example/mprog/MainActivity.kt
+++ 2.1/app/src/main/java/com/example/mprog/MainActivity.kt
@@ -14,26 +14,27 @@ import kotlin.math.sqrt
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity";
-
+    private val counterFile: File = primeCounterFile();
+    private lateinit var mEditText: EditText;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        primeCounter();
+
+        val readPrime = Integer.parseInt(counterFile.readText()).toLong();
+        mEditText = findViewById<EditText>(R.id.edit_text);
+
     }
 
-    fun primeCounter(){
-        val fileName = "primecounter"
-        val initialValue = 1
+    fun primeCounterFile() : File{
+        val fileName = "primecounter";
+        return File(this.filesDir, fileName);
 
-        var primeCounterFile = File(this.filesDir, fileName);
-        Log.d(TAG, "File path ${primeCounterFile.path}");
     }
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
-        val message = editText.text.toString()
+
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
@@ -51,6 +52,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun findNextPrime(current: Long){
-        if
+
+        if (isPrime(current)){
+            counterFile.writeText(current.toString());
+        }
     }
 }
