252cb5b28820d35d788c790b6b8e2e47aef424be
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-08-22 16:32:31 +0200

    Program finally succeeded

--- 2.1/app/src/main/java/com/example/mprog/MainActivity.kt
+++ 2.1/app/src/main/java/com/example/mprog/MainActivity.kt
@@ -22,49 +22,56 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mEditText = findViewById<EditText>(R.id.edit_text);
+        mEditText?.setText(getFilePrime().toString());
     }
 
     private fun getFilePrime() : Long{
         val fileName = "primecounter.txt";
-        var currentPrimeFile = File(this.filesDir, fileName)
-        var primeFileWriter = FileWriter(currentPrimeFile)
+        var currentPrimeFile = File(this.getFilesDir(), fileName)
         var currentPrime = currentPrimeFile.readText()
 
-        if (currentPrime == ""){
-            primeFileWriter?.write(3.toString())
-            primeFileWriter.close()
+        if (currentPrime == "" || currentPrime == null){
+            currentPrimeFile.writeText(3.toString())
             return 3.toLong()
         }
         return currentPrime!!.toLong()
     }
 
     private fun isPrime(candidate: Long): Boolean{
-        var sqrt: Long = sqrt(candidate.toDouble()).toLong();
-        var i: Long = 3;
-        while(i < sqrt) {
-            if ((candidate % i) == 0.toLong()) {
-                return false;
-            }
-            i += 2
-        }
 
-        return true;
+        var flag : Boolean = false
+        var i: Long = 2;
+        while(i <= (candidate / 2)) {
+            if ((candidate % i) == 0.toLong()) {
+                flag = true
+                break
+            }
+            i ++
+        }
+        if (!flag){
+            return true
+        }
+        return false
+
     }
 
     fun findNextPrime(view : View){
         var currentPrime = getFilePrime().toLong()
-        if(isPrime(currentPrime)){
-            this.mEditText?.setText(currentPrime.toString());
-            currentPrime += 2
-
-            val fileName = "primecounter.txt";
-            var currentPrimeFile = File(this.filesDir, fileName)
-            var primeFileWriter = FileWriter(currentPrimeFile)
-
-            primeFileWriter?.write(currentPrime.toString())
-            primeFileWriter?.close()
+        while(true) {
+            if (isPrime(currentPrime)) {
+                this.mEditText?.setText(currentPrime.toString());
+                currentPrime += 2
+                val fileName = "primecounter.txt";
+                var currentPrimeFile = File(this.getFilesDir(), fileName)
+                currentPrimeFile.writeText(currentPrime.toString())
+                Log.d(TAG, "passed ###")
+                break;
+            }
+            else {
+                currentPrime += 2
+            }
         }
-
+        Log.d(TAG, "Current prime is ${currentPrime}")
     }
 
 }
