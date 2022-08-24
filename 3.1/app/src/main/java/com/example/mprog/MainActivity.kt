68ddec434b4bb23ba106bb4443e4ebbf96caca93
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-08-24 13:12:42 +0200

    v2

--- 3.1/app/src/main/java/com/example/mprog/MainActivity.kt
+++ 3.1/app/src/main/java/com/example/mprog/MainActivity.kt
@@ -1,94 +1,48 @@
 package com.example.mprog
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.core.content.FileProvider
-import java.io.File
-import java.io.IOException
-import java.sql.Timestamp
-import java.text.SimpleDateFormat
-import java.util.*
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
+import android.Manifest.permission
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
 
-private const val REQUEST_CODE = 7
-
-const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity";
-    private val button = findViewById<Button>(R.id.btnTakePicture)
-    private val imageContainer = findViewById<ImageView>(R.id.imageView)
-    lateinit var currentPhotoPath: String
+
+    val REQUEST_CODE = 7
+    lateinit var imageView : ImageView
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            TakeImage()
-        }
+        imageView = findViewById(R.id.image_view)
+        button = findViewById(R.id.camera_button)
 
-    }
+        button.setOnClickListener{
+            val takePictureIntent : Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File{
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-
-        return File.createTempFile(
-            "JPEG_${timeStamp}_",
-            ".jpg",
-            storageDir
-        ).apply {
-            currentPhotoPath = absolutePath
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
-            val imageBitMap = data?.extras?.get("data") as Bitmap
-            imageContainer.setImageBitmap(imageBitMap)
-        }
-        else{
-            super.onActivityResult(requestCode, resultCode, data)
-        }
-    }
-
-    private fun TakeImage(){
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
-            takePictureIntent ->
-
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                val photoFile: File? = try {
-                    createImageFile()
-                }catch (ex : IOException){
-                    null
-                }
-
-                photoFile?.also{
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this,
-                        "com.example.android.fileprovider",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, REQUEST_CODE)
-                }
+            if(takePictureIntent.resolveActivity(this.packageManager) != null){
+                startActivityForResult(takePictureIntent, REQUEST_CODE)
             }
+
+            println("working?")
         }
     }
 
-
-
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val bitmap: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(bitmap)
+        }
+    }
 }
