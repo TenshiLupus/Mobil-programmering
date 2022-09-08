c2ca789b329894f1ff3cde252c33e4be2bce9bab
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-09-08 12:07:36 +0200

    button action added

--- 9/app/src/main/java/com/example/memoryapp/MemoryBoardAdapter.kt
+++ 9/app/src/main/java/com/example/memoryapp/MemoryBoardAdapter.kt
@@ -1,16 +1,34 @@
 package com.example.memoryapp
 
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(private val context : Context, private val numPieces : Int) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>(){
 
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view : View = LayoutInflater.from(context).inflate(R.layout.m_card, parent, false)
+        val cardWith = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLength = min(cardWith, cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.m_card, parent, false)
+
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
         return ViewHolder(view)
     }
 
@@ -21,8 +39,11 @@ class MemoryBoardAdapter(private val context : Context, private val numPieces : 
     override fun getItemCount() = numPieces
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
         fun bind(position : Int){
-            //no-op
+            imageButton.setOnClickListener{
+                Log.i(TAG, "Clicked on position ${position}")
+            }
         }
     }
 }
