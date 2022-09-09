c0f9acb2c72ba1275411f5cb06cbf92511dfa139
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-09-09 13:11:08 +0200

    game state added

--- 9/app/src/main/java/com/example/memoryapp/MainActivity.kt
+++ 9/app/src/main/java/com/example/memoryapp/MainActivity.kt
@@ -1,35 +1,67 @@
 package com.example.memoryapp
 
+import android.app.GameManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.memoryapp.models.BoardSize
+import com.example.memoryapp.models.MemoryCard
 import com.example.memoryapp.utils.DEFAULT_ICONS
+import com.example.memoryapp.utils.MemoryGame
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var clRoot : ConstraintLayout
     private lateinit var recyclerViewBoard : RecyclerView
     private lateinit var textViewNumberMoves : TextView
     private lateinit var textViewNumberPairs : TextView
 
     private var boardSize : BoardSize = BoardSize.EASY
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter : MemoryBoardAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clRoot = findViewById(R.id.clRoot)
         recyclerViewBoard = findViewById(R.id.RecycleViewBoard)
         textViewNumberMoves = findViewById(R.id.textViewNumberMoves)
         textViewNumberPairs = findViewById(R.id.textViewNumberPairs)
 
-        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
-        val randomizedImages = (chosenImages + chosenImages).shuffled()
-        randomizedImages.q
+        memoryGame = MemoryGame(boardSize)
         
-        recyclerViewBoard.adapter = MemoryBoardAdapter(this,boardSize, randomizedImages)
+        adapter = MemoryBoardAdapter(this,boardSize, memoryGame.cards, object : MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position : Int){
+                updateGameWithFlip(position)
+            }
+        })
+
+        recyclerViewBoard.adapter = adapter
         recyclerViewBoard.setHasFixedSize(true)
         recyclerViewBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun updateGameWithFlip(position : Int) {
+
+        if(memoryGame.haveWonGame()){
+            Snackbar.make(clRoot, "You already won!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        if(memoryGame.isCardFaceUp(position)){
+            Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        memoryGame.flipCard(position)
+        adapter.notifyDataSetChanged()
     }
 
 }
