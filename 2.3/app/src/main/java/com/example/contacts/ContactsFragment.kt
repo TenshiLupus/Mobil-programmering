63a54981b0dbd5f2fc50fa41eea9cd7d5298093b
Tenshilupus <Angel__CM@hotmail.com>
2023-01-05 17:56:05 +0100

    Contact List fragment

--- 2.3/app/src/main/java/com/example/contacts/ContactsFragment.kt
+++ 2.3/app/src/main/java/com/example/contacts/ContactsFragment.kt
@@ -6,15 +6,13 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.loader.app.LoaderManager
-import android.widget.AdapterView
-import android.widget.CursorAdapter
-import android.widget.ListView
-import android.widget.SimpleCursorAdapter
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import java.lang.IllegalStateException
@@ -30,6 +28,7 @@ private val FROM_COLUMNS: Array<String> = arrayOf(
 
 @SuppressLint("InlineApi")
 private val PROJECTION : Array<out String> = arrayOf(
+
     ContactsContract.Contacts._ID,
     ContactsContract.Contacts.LOOKUP_KEY,
     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
@@ -45,17 +44,12 @@ private val SELECTION: String =
     else
         "${ContactsContract.Contacts.DISPLAY_NAME} LIKE ?"
 
-private val searchString : String = TODO()
-private val selectionArgs = arrayOf<String>(searchString)
-
-
-
 private const val CONTACT_ID_INDEX : Int = 0
 private const val CONTACT_KEY_INDEX : Int = 1
 
 private val TO_IDS: IntArray = intArrayOf(android.R.id.text1)
 
-class ContactsFragment : Fragment(),LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener{
+class ContactsFragment : Fragment(R.layout.contact_list_fragment), LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener{
 
     lateinit var contactsList: ListView
     var contactId : Long = 0
@@ -64,78 +58,54 @@ class ContactsFragment : Fragment(),LoaderManager.LoaderCallbacks<Cursor>, Adapt
 
     private var cursorAdapter: SimpleCursorAdapter? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container : ViewGroup?, savedInstanceState: Bundle?
-    ) : View?{
-        return inflater.inflate(R.layout.contact_list_fragment, container, false)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        loaderManager.initLoader(0, null, this)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         activity?.also {
-            contactsList = it.findViewById(android.R.id.text1)
-            cursorAdapter = SimpleCursorAdapter(
-                it, R.layout.contacts_list_item, null, FROM_COLUMNS, TO_IDS, 0
-            )
+            contactsList = it.findViewById(R.id.fragment_list_toadd) as ListView
+            cursorAdapter = SimpleCursorAdapter(it, R.layout.contacts_list_item, null, FROM_COLUMNS, TO_IDS, 0)
             contactsList.adapter = cursorAdapter
         }
-
         contactsList.onItemClickListener = this
-
-        loaderManager.initLoader(0,null, this)
-    }
-
-    override fun <D : Any?> initLoader(
-        id: Int,
-        args: Bundle?,
-        callback: LoaderManager.LoaderCallbacks<D>
-    ): Loader<D> {
-        TODO("Not yet implemented")
-    }
-
-    override fun <D : Any?> restartLoader(
-        id: Int,
-        args: Bundle?,
-        callback: LoaderManager.LoaderCallbacks<D>
-    ): Loader<D> {
-        TODO("Not yet implemented")
-    }
-
-    override fun destroyLoader(id: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun <D : Any?> getLoader(id: Int): Loader<D>? {
-        TODO("Not yet implemented")
-    }
-
-    override fun markForRedelivery() {
-        TODO("Not yet implemented")
     }
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
 
+        val searchString : String? = requireArguments().getString("searchName")
+        val selectionArgs = arrayOf(searchString)
+
+        Toast.makeText(activity, searchString, Toast.LENGTH_SHORT).show()
+        Log.i("myatag", "searchString is : $searchString")
         selectionArgs[0] = "%$searchString%"
 
         return activity?.let {
+
             return CursorLoader(it,ContactsContract.Contacts.CONTENT_URI, PROJECTION, SELECTION, selectionArgs, null)
         } ?: throw IllegalStateException()
     }
 
-    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        TODO("Not yet implemented")
+    override fun onLoadFinished(loader: Loader<Cursor>, cursor: Cursor) {
+        cursorAdapter?.swapCursor(cursor)
+
     }
 
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        TODO("Not yet implemented")
+        cursorAdapter?.swapCursor(null)
     }
 
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val cursor : Cursor? = (parent?.adapter as? CursorAdapter)?.cursor?.apply {
+    override fun onItemClick(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        val cursor : Cursor? = (parent.adapter as? CursorAdapter)?.cursor?.apply {
             moveToPosition(position)
             contactId = getLong(CONTACT_ID_INDEX)
             contactKey = getString(CONTACT_KEY_INDEX)
             contactUri = ContactsContract.Contacts.getLookupUri(contactId, contactKey)
         }
+
+        Log.i("myatag", "An item has been clicked")
     }
 }
--- 2.3/app/src/main/java/com/example/contacts/MainActivity.kt
+++ 2.3/app/src/main/java/com/example/contacts/MainActivity.kt
@@ -1,14 +1,71 @@
 package com.example.contacts
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val PERMISSIONS_REQUEST_READ_CONTACTS = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf<String>(Manifest.permission.READ_CONTACTS), PERMISSIONS_REQUEST_READ_CONTACTS);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        }
+
+        var button = this@MainActivity.findViewById(R.id.button) as Button
+
+        button.setOnClickListener {
+            if(savedInstanceState == null){
+                val editText = this@MainActivity.findViewById<EditText>(R.id.editTextTextPersonName2)
+                var searchName = editText.text.toString()
+
+                var bundle = bundleOf("searchName" to searchName)
+                supportFragmentManager.commit {
+
+                    setReorderingAllowed(true)
+                    add<ContactsFragment>(R.id.fragment_container_view, args = bundle)
+                }
+            }
+            Toast.makeText(this, "PLEASE WORK", Toast.LENGTH_LONG).show()
+            Log.i("myatag", "CLICKING")
+        }
     }
+
+    fun proceed(savedInstanceState: Bundle?){
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                return
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
 }
+
