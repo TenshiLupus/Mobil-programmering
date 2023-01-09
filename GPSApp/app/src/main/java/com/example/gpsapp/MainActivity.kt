124064bff44c96369dc240a956c4d1bbf023c12d
Tenshilupus <Angel__CM@hotmail.com>
2023-01-09 22:26:59 +0100

    GPS INIT

--- GPSApp/app/src/main/java/com/example/gpsapp/MainActivity.kt
+++ GPSApp/app/src/main/java/com/example/gpsapp/MainActivity.kt
@@ -2,28 +2,42 @@ package com.example.gpsapp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+
+
+val RCS = 100
+
+val locationRequest = LocationRequest.create()?.apply {
+    interval = 10000
+    fastestInterval = 5000
+    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+}
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient : FusedLocationProviderClient
-
+    private var requestingLocationUpdates = false
+    private lateinit var locationCallback : LocationCallback
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        updateValuesFromBundle(savedInstanceState)
 
         val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions ->
             when{
@@ -39,20 +53,81 @@ class MainActivity : AppCompatActivity() {
         }
 
         requestPermissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
+        createLocationRequest()
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         fusedLocationClient.lastLocation.addOnSuccessListener { location ->
 
         }
-    }
 
-    fun locationRequest(){
-        val locationRequest = LocationRequest.create()?.apply {
-            interval = 10000
-            fastestInterval = 5000
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(p0: LocationResult) {
+
+                for (location in p0.locations){
+                    Log.i("locations", location.toString())
+                }
+
+            }
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        if(requestingLocationUpdates){
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates(){
+        fusedLocationClient.requestLocationUpdates(locationRequest!!, locationCallback, Looper.getMainLooper())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+
+    fun createLocationRequest(){
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest!!)
+
+        val client : SettingsClient = LocationServices.getSettingsClient(this)
+        val task : Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingResponse ->
+
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                try{
+                    exception.startResolutionForResult(this@MainActivity, RCS)
+                } catch (sendEx : IntentSender.SendIntentException){
+                    //ignore error
+                }
+            }
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("LUK", requestingLocationUpdates)
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun updateValuesFromBundle(savedInstanceState: Bundle?) {
+        savedInstanceState ?: return
+
+        // Update the value of requestingLocationUpdates from the Bundle.
+        if (savedInstanceState.keySet().contains("LUK")) {
+            requestingLocationUpdates = savedInstanceState.getBoolean("LUK")
+        }
+    }
 }
